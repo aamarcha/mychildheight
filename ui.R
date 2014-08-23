91ce7c8cdb805fcc0ebfef7b3f9dff71600411b1
Antoine Aamarcha <a.aamarcha@gmail.com>
2014-08-23 16:58:10 +0200

    look and feel UI

--- ui.R
+++ ui.R
@@ -2,7 +2,7 @@ library(shiny)
 shinyUI(
         navbarPage("MyChildHeight",
                    tabPanel("Prediction"),
-                   h1('You can predict your child Height '),
+                   h1('You can predict your child height ', class="text-center text-info"),
                    h3("Who is Galton ?"),
                    p( a(href="http://en.wikipedia.org/wiki/Francis_Galton","Francis Galton")," (born on 1822), a pioneer in statistics, pretend to predict your child height. In this Shiny App, let's challenge Francis :) "),
                    h3("How you can use this App?"),
@@ -10,20 +10,21 @@ shinyUI(
                            tags$li("First, enter your height in cm "),
                            tags$li("Then, click on 'Predict' button to see your predicted child height")
                            ),
+                   h3("Let's challenge Francis!"),
                    tags$hr(),
                    pageWithSidebar( 
-                           headerPanel("Let's challenge Francis!"),
+                           headerPanel(""),
                            sidebarPanel(
-                                   numericInput('parentHeight', 'Your height cm', 165, min =100, max = 250, step = 1),
-                                   span("Values between 100cm and 250cm", class= "small"),
+                                   numericInput('parentHeight', 'Your height (cm)', 165, min =100, max = 250, step = 1),
+                                   p("Values between 100cm and 250cm", class= "small"),
                                    submitButton('Predict')
                            ),
                            mainPanel(
                          
-                                   h3('Prediction'), 
-                                   h4('Your height in cm :'), 
+                                   h3('Prediction',class="text-warning"), 
+                                   h4('Your height (cm) :', class="text-success"), 
                                    verbatimTextOutput("inputValue"), 
-                                   h4('you have a 97% chance of having a child  height:'), 
+                                   h4('You have a chance of 95% of having a child  height:',class="text-success"), 
                                    verbatimTextOutput("prediction"),
                                    plotOutput('parentVSchild')
                            ) 
